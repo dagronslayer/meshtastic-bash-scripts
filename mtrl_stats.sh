73b84aa7e6381b2c8b4a1b657b542d54997e35d5
dagronslayer <74074460+dagronslayer@users.noreply.github.com>
2025-05-01 10:48:49 -0600

    Update mtrl_stats.sh
    include both outbound and inbound SNR values (only the first and last hop)

--- mtrl_stats.sh
+++ mtrl_stats.sh
@@ -11,8 +11,13 @@ if [ ! -f ./mtrl_log/mtrd.log ] || [ ! -f ./mtrl_log/mtrl.log ]; then
     exit 1
 fi
 
+echo "Outbound SNR values:"
+grep -i -A 1 'Route traced towards destination' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '^[^)]*' | cut -d '(' -f 2 | grep -v '?dB'
+osnrc=$(grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '[^(]*$' | cut -d ')' -f 1 | grep -v '?dB' | wc -l)
+echo ""
+echo "Inbound SNR values:"
 grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '[^(]*$' | cut -d ')' -f 1 | grep -v '?dB'
-snrc=$(grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '[^(]*$' | cut -d ')' -f 1 | grep -v '?dB' | wc -l)
+isnrc=$(grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '[^(]*$' | cut -d ')' -f 1 | grep -v '?dB' | wc -l)
 
 success=$(cat ./mtrl_log/mtrl.log | grep "success" | wc -l)
 fail=$(cat ./mtrl_log/mtrl.log | grep "fail" | wc -l)
@@ -24,6 +29,41 @@ echo "Failed traces: $fail"
 echo "$(( 100*$success/$total ))% Success"
 
 hold=true
+for i in $(grep -i -A 1 'Route traced towards destination' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '^[^)]*' | cut -d '(' -f 2 | cut -d 'd' -f 1 | grep -v '?' | grep -v '-'); do
+    if [ "$hold" = true ]; then
+        o="$i"
+        hold=false
+    else
+        o="$o + $i"
+    fi
+done
+if [[ -n "$o" ]]; then
+    opsnrt=$(echo "$o" | bc)
+else
+    opsnrt="0"
+fi
+
+hold=true
+for i in $(grep -i -A 1 'Route traced towards destination' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '^[^)]*' | cut -d '(' -f 2 | cut -d 'd' -f 1 | grep -v '?' | grep '-'); do
+    if [ "$hold" = true ]; then
+        o="$i"
+        hold=false
+    else
+        o="$o + $i"
+    fi
+done
+if [[ -n "$o" ]]; then
+    onsnrt=$(echo "$o" | bc)
+else
+    onsnrt="0"
+fi
+
+osnrt=$(echo "$opsnrt + $onsnrt" | bc)
+osnrtm=$(echo "scale=2; $osnrt / $osnrc" | bc)
+
+echo "Outbound: ${osnrtm}dB mean SNR from ${osnrc} recorded values"
+
+hold=true
 for i in $(grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'-->' | grep -o '[^(]*$' | cut -d 'd' -f 1 | grep -v '?' | grep -v '-'); do
     if [ "$hold" = true ]; then
         o="$i"
@@ -33,9 +73,9 @@ for i in $(grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'
     fi
 done
 if [[ -n "$o" ]]; then
-    psnrt=$(echo "$o" | bc)
+    ipsnrt=$(echo "$o" | bc)
 else
-    psnrt="0"
+    ipsnrt="0"
 fi
 
 hold=true
@@ -48,12 +88,12 @@ for i in $(grep -i -A 1 'Route traced back to us' ./mtrl_log/mtrd.log | grep \\'
     fi
 done
 if [[ -n "$o" ]]; then
-    nsnrt=$(echo "$o" | bc)
+    insnrt=$(echo "$o" | bc)
 else
-    nsnrt="0"
+    insnrt="0"
 fi
 
-snrt=$(echo "$psnrt + $nsnrt" | bc)
-snrtm=$(echo "scale=2; $snrt / $snrc" | bc)
+isnrt=$(echo "$ipsnrt + $insnrt" | bc)
+isnrtm=$(echo "scale=2; $isnrt / $isnrc" | bc)
 
-echo "${snrtm}dB mean SNR from ${snrc} recorded values"
+echo "Inbound: ${isnrtm}dB mean SNR from ${isnrc} recorded values"
