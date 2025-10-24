e0cc4bb2af421e20d46d5c936cdb57e42807ca4e
dagronslayer <74074460+dagronslayer@users.noreply.github.com>
2025-10-24 11:07:39 -0600

    Update mtr.sh

--- mtr.sh
+++ mtr.sh
@@ -2,7 +2,7 @@
 mnode=$(meshtastic --nodes --show-fields user.id,user.shortName,user.longName | grep "$1")
 if [ -n "$mnode" ]; then
         echo "$mnode"
-        madd=$(echo "$mnode" | awk -F'│' '{print $3}' | xargs)
+        madd=$(echo "$mnode" | head -1 | awk -F'│' '{print $3}' | xargs)
         meshtastic --traceroute "$madd"
 else
         echo "$1 not found"
