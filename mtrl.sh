abeaf912692363601db96227d87f6e82debf5247
jeepnjonny <40327904+jeepnjonny@users.noreply.github.com>
2025-12-30 13:28:35 -0700

    added command line options

--- mtrl.sh
+++ mtrl.sh
@@ -1,22 +1,63 @@
 #!/bin/bash
 
-# Interval between traces. Default is 900 seconds (15 minutes). Default iterations is 48 (12 hours).
-interval="900"
-iterations="48"
+# Define usage function
+usage() {
+    echo "Usage: $0 [-i interval] [-r repeat] [-p port] <required_argument>"
+    exit 1
+}
 
+# Initialize variables
+PORT="" # Set a default value for the port
+INTERVAL="900" # default interval 15 minutes
+REPEAT="48" # default iterations 12h at 15m intervals
+
+# Process flagged options
+while getopts "i®p:" opt; do
+  case $opt in
+    i) INTERVAL="$OPTARG" ;;
+    r) REPEAT="$OPTARG" ;;
+    p) PORT="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+# 3. Shift the arguments
+# This moves the positional parameters so the required argument becomes $1
+shift $((OPTIND - 1))
+
+# 4. Handle the required positional parameter
+NODE=$1
+
+if [[ -z "$NODE" ]]; then
+    echo "Error: Missing node identifier"
+    usage
+fi
+
+# the meat of the script
 [ ! -d ./mtrl_log ] && mkdir ./mtrl_log
 if [ -f ./mtrl_log/mtrl.log ]; then
     read -e -p "Clear current logs? (y/n) " choice
     [[ "$choice" == [Yy]* ]] && rm ./mtrl_log/*.log
 fi
 
-mnode=$(meshtastic --nodes --show-fields user.id,user.shortName,user.longName | grep "$1")
+# get the node identifier
+if [[ -n "$PORT" ]]; then
+  mnode=$(meshtastic --port $PORT --nodes --show-fields user.id,user.shortName,user.longName | grep "$NODE")
+else
+  mnode=$(meshtastic --nodes --show-fields user.id,user.shortName,user.longName | grep "$NODE")
+  PORT="default"
+fi
+
+# validate with the user the parameters being used
+echo "Target: $NODE"
+echo "Config: Interval=$INTERVAL, Repeat=$REPEAT, Port=$PORT"
+
+# loop through a Traceroute $REPEAT times
 if [ -n "$mnode" ]; then
         echo "$mnode"
         madd=$(echo "$mnode" | awk -F'│' '{print $3}' | xargs)
-        for(( i=$iterations; i>0; i-=1 )); do
+        for(( i=$REPEAT; i>0; i-=1 )); do
             mtb=$(date +%s)
-            mout=$(meshtastic --traceroute "$madd")
+            mout=$(meshtastic --port $PORT --traceroute "$madd")
             mte=$(date +%s)
             mtt=$(expr $mte - $mtb)
             mout2=$(echo $mout | grep "Aborting")
@@ -26,8 +67,8 @@ if [ -n "$mnode" ]; then
                 echo "${mtb}|${1}|success|${mtt}" >> ./mtrl_log/mtrl.log
             fi
             echo "${mtb}|${mout}" >> ./mtrl_log/mtrd.log
-            sleep "$interval";
+            sleep "$INTERVAL";
         done
 else
-        echo "$1 not found"
+        echo "$NODE not found"
 fi
