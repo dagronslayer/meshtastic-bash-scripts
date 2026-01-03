86f69454ec8d13306e5106aba24e113cdcb3ec35
dagronslayer <74074460+dagronslayer@users.noreply.github.com>
2026-01-03 01:24:41 -0700

    Add files via upload

---
+++ ignore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mnode=$(meshtastic --nodes --show-fields user.id,user.shortName,user.longName | grep "$1")
+if [ -n "$mnode" ]; then
+        echo "$mnode"
+        madd=$(echo "$mnode" | head -1 | awk -F'│' '{print $3}' | xargs)
+        if [[ -f "ra.lst" ]]; then
+            while IFS= read -r radd; do
+                out=$(meshtastic --set-ignored-node "$madd" --dest "$radd")
+            done < "ra.lst"
+        else
+            echo "ra.lst not found"
+            exit 1
+        fi
+        sleep 30
+else
+        echo "$1 not found"
+fi
